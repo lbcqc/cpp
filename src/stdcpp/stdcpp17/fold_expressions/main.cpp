0ea4a1da869c7bb43777d211e376ed88bfd99ab2
howarli <howarli@tencent.com>
2024-04-11 00:29:34 +0800

    add stdcpp some demo

--- src/stdcpp/stdcpp17/fold_expressions/main.cpp
+++ src/stdcpp/stdcpp17/fold_expressions/main.cpp
@@ -8,7 +8,13 @@ auto sum(T... args) {
   return (args + ...);
 }
 
+template <typename... T>
+auto mean(T... args) {
+  return (args + ...) / sizeof...(args);
+}
+
 int main(int argc, char **argv) {
   std::cout << sum(1, 2, 3, 4, 5, 6, 7, 8, 9.0, 0.01) << std::endl;
+  std::cout << mean(1, 2, 3, 4, 5, 6, 7, 8, 9.0, 0.01) << std::endl;
   return 0;
 }
---
+++ src/stdcpp/stdcpp17/structured_binding/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by root on 4/11/24.
+//
+#include <iostream>
+#include <map>
+
+int main(int argc, char *argv[]) {
+  std::map<std::string, int> m = {
+    { "a", 1 },
+    { "b", 2 },
+    { "c", 3 },
+  };
+
+  for (auto &&[k, v] : m) {
+    std::cout << k << " " << v << '\n';
+  }
+  return 0;
+}
