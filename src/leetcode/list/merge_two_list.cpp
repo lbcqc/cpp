42606476c2e5d6e683025c56864cb37ba24e5a4e
howarli <howarli@tencent.com>
2025-09-09 23:22:47 +0800

    fix predefine errror

--- src/leetcode/list/merge_two_list.cpp
+++ src/leetcode/list/merge_two_list.cpp
@@ -10,38 +10,38 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-// 迭代
-class Solution {
-public:
-  ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-    ListNode dummy;
-    ListNode *list = &dummy;
-    while (list1 != nullptr && list2 != nullptr) {
-      if (list1->val <= list2->val) {
-        list->next = list1;
-        list1 = list1->next;
-      } else {
-        list->next = list2;
-        list2 = list2->next;
-      }
-      list = list->next;
-    }
-    // while (list1 != nullptr) {
-    //   list->next = list1;
-    //   list = list->next;
-    //   list1 = list1->next;
-    // }
-    // while (list2 != nullptr) {
-    //   list->next = list2;
-    //   list = list->next;
-    //   list2 = list2->next;
-    // }
-    // list->next = nullptr;
-    // 合并之后最多还有一个链表未被遍历完，直接追加即可
-    list->next = list1 == nullptr? list2:list1;
-    return dummy.next;
-  }
-};
+// // 迭代
+// class Solution {
+// public:
+//   ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//     ListNode dummy;
+//     ListNode *list = &dummy;
+//     while (list1 != nullptr && list2 != nullptr) {
+//       if (list1->val <= list2->val) {
+//         list->next = list1;
+//         list1 = list1->next;
+//       } else {
+//         list->next = list2;
+//         list2 = list2->next;
+//       }
+//       list = list->next;
+//     }
+//     // while (list1 != nullptr) {
+//     //   list->next = list1;
+//     //   list = list->next;
+//     //   list1 = list1->next;
+//     // }
+//     // while (list2 != nullptr) {
+//     //   list->next = list2;
+//     //   list = list->next;
+//     //   list2 = list2->next;
+//     // }
+//     // list->next = nullptr;
+//     // 合并之后最多还有一个链表未被遍历完，直接追加即可
+//     list->next = list1 == nullptr? list2:list1;
+//     return dummy.next;
+//   }
+// };
 
 // 递归
 class Solution {
