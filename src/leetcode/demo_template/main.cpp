1abe44cc10e2e169c3d5c5c004a3151ef364f093
howarli <howarli@tencent.com>
2026-02-12 14:37:24 +0800

    fix main.cpp

--- src/leetcode/demo_template/main.cpp
+++ src/leetcode/demo_template/main.cpp
@@ -7,16 +7,41 @@
 #include <string>
 #include <tuple>
 #include <vector>
+#include <unordered_set>
 
 #include "spdlog/fmt/ranges.h"
 #include "spdlog/spdlog.h"
 
-int main() {
-  std::list<int> l = {1, 2, 3, 4, 5};
-  std::_List_iterator<int> a2 = find(l.begin(), l.end(), 2);
-  std::_List_iterator<int> a4 = find(l.begin(), l.end(), 4);
-  for (auto it = a2; it != a4; it++) {
-    spdlog::info("{}->", *it);
+using namespace std;
+
+class Solution {
+public:
+  bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<unordered_set<int>> dq(numCourses);
+    for (int i = 0; i < prerequisites.size(); i++) {
+      for (int j = 1; j < prerequisites[i].size(); j++) {
+        if (dq[prerequisites[i][j]].find(prerequisites[i][0]) != dq[prerequisites[i][j]].end()) {
+          return false;
+        }
+        dq[prerequisites[i][0]].insert(prerequisites[i][j]);
+        dq[prerequisites[i][0]].insert(dq[prerequisites[i][j]].begin(), dq[prerequisites[i][j]].end());
+      }
+    }
+    for (int i = 0; i < numCourses; i++) {
+      if (dq[i].find(i) != dq[i].end()) {
+        return false;
+      }
+    }
+    return true;
   }
+};
+
+int main() {
+  Solution s;
+  vector<vector<int>> prerequisites{{1,0}, {0,1}};
+  cout << s.canFinish(2, prerequisites) << endl;
+  // for (auto it = a2; it != a4; it++) {
+  //   spdlog::info("{}->", *it);
+  // }
   return 0;
 }
